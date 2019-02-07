3944fe2119767122ff2ae258ac04292b86f24602
Calvin Nguyen <cuongvqnguyen@gmail.com>
2019-02-06 17:54:01 -0800

    [finish spec] Check the type of the Triangle
    update README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # SE187-Software-QA
-This is a group repo, which is created to work on Software Quality Assurance 
+
+## Installation
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and JUnit installed
+
+Clone this repository
+```
+git clone https://github.com/NivlaCuong/SE187-Software-QA.git
+```
+
+##  Project Composition: 
+
+```
+├── src/                    - Storing all projects of the course 
+├── .gitignore              - Files that is not allowed to push on Git 
+├── README.md               - Information of the project 
+└── SE187-Software-QA.iml    
+```
+
+## Authors
+**Calvin Nguyen** - [NivlaCuong](https://github.com/NivlaCuong) 
+
+**Cheng Chin Lim** - [Cheng Chin Lim](https://github.com/chenggarnett)
+
+**Sanford huynh** - [Sanford Huynh](https://github.com/NivlaCuong)
+
+## License 
+Use of this source code is governed by an MIT-style license.
---
+++ src/TriangleChecker/TriangleCheckerTest.java
@@ -0,0 +1,99 @@
+package TriangleChecker;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ */
+public class TriangleCheckerTest {
+
+	/**
+	 *
+	 */
+	@Test
+	public void test() {
+
+		checkTrianglesWithAtLeastTwoDiffLength();
+
+		checkEquilateralTriangles();
+
+		checkWithTwoSameSide();
+
+		checkWithOneNegativeNumber();
+
+	}
+
+	/**
+	 * Check The Triangle with 2 fixed different side's length Y = 4 and Z = 3 with X is a number from -10 to 10
+	 */
+	private void checkTrianglesWithAtLeastTwoDiffLength() {
+		// check if the triangle has at least two different lengths
+		for (int i = - 10; i < 10; i++) {
+			TriangleChecker triangleChecker = new TriangleChecker(i,4,3);
+			triangleChecker.checkTriangleType();
+			if (i < 2) { // (0, 4, 3), (1, 4, 3)
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
+			} else if (i >= 7) {
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
+			} else if (i == 3 || i == 4) { // (3, 4, 3), (4, 4, 3)
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.ISOSCELES);
+			} else { // others
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.SCALENE);
+			}
+		}
+	}
+
+	/**
+	 *
+	 */
+	private void checkEquilateralTriangles() {
+		for (int i = - 100; i < 100; i++) {
+			TriangleChecker triangleChecker = new TriangleChecker(i,i,i);
+			triangleChecker.checkTriangleType();
+			if (i <= 0) {
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
+			} else {
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.EQUILATERAL);
+			}
+		}
+	}
+
+	/**
+	 * Run with 2 equal value
+	 */
+	private void checkWithTwoSameSide() {
+		for(int i = -100; i < 100; i++){
+			TriangleChecker triangleChecker2 = new TriangleChecker(i,10,10);
+			triangleChecker2.checkTriangleType();
+			if(i < 1){
+				assertEquals(triangleChecker2.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
+			}else if(i >= 20){
+				assertEquals(triangleChecker2.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
+
+			}else if(i == 10){
+				assertEquals(triangleChecker2.getTriangle().getTriangleType(), TriangleChecker.TriangleType.EQUILATERAL);
+
+			}else {
+				assertEquals(triangleChecker2.getTriangle().getTriangleType(), TriangleChecker.TriangleType.ISOSCELES);
+			}
+
+		}
+	}
+
+	/**
+	 *
+	 */
+	private void checkWithOneNegativeNumber() {
+		for(int i = -100; i < 100; i++){
+			TriangleChecker triangleChecker3 = new TriangleChecker(i,10,-7);
+			triangleChecker3.checkTriangleType();
+			if(i < -10){
+				assertEquals(triangleChecker3.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
+			}else
+				assertEquals(triangleChecker3.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
+
+		}
+	}
+
+}
