061de2250a512b3a3abba072a7e9b5d71c5f4bd2
chenggarnett <chengchin5227@gmail.com>
2019-02-06 22:06:17 -0800

    Sorry I wrote a for loop test case, not sure if it's enough

--- src/TriangleChecker/TriangleCheckerTest.java
+++ src/TriangleChecker/TriangleCheckerTest.java
@@ -12,15 +12,15 @@ public class TriangleCheckerTest {
 		for (int i = 0; i < 10; i++) {
 			TriangleChecker triangleChecker = new TriangleChecker(i,4,3);
 			triangleChecker.checkTriangleType();
-//			if (i < 1) {
-//				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
-//			} else if (i >= 7) {
-//				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
-//			} else if (i == 3 || i == 4) {
-//				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.ISOSCELES);
-//			} else {
-//				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.SCALENE);
-//			}
+			if (i < 2) {
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
+			} else if (i >= 7) {
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.INVALID);
+			} else if (i == 3 || i == 4) {
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.ISOSCELES);
+			} else {
+				assertEquals(triangleChecker.getTriangle().getTriangleType(), TriangleChecker.TriangleType.SCALENE);
+			}
 		}
 		
 	}
