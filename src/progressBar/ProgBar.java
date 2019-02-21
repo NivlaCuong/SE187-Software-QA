ca8a745f2b84470a79261ec9b30e01bcc016abc4
chenggarnett <chengchin5227@gmail.com>
2019-02-20 17:33:42 -0800

    try to understand the question

---
+++ src/progressBar/ProgBar.java
@@ -0,0 +1,14 @@
+package progressBar;
+
+public class ProgBar {
+	
+	public static String printColor(double percentage, double x, double y) {
+		double degrees = percentage / 100 * 360;
+		return null;
+	}
+	
+	public static void main(String args[]) {
+		// below code is just for testing, should be tested more thoroughly using JUnit
+		
+	}
+}
